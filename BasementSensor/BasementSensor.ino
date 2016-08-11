33c06842e01571d9947faa903abe6c70a1e9d5da
Tim Lampman <timothy.lampman@gmail.com>
2016-08-10 22:48:51 -0400

    Add project files.

---
+++ BasementSensor/BasementSensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BasementSensor.ino
+ Created:	8/10/2016 10:48:42 PM
+ Author:	tl1
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
